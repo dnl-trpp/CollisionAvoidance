67606e0425802c79b97ec166790ac4b60f966564
dnl-trpp <daniel.trippa@gmail.com>
2021-09-12 23:33:40 +0200

    Debbugging Laser Scan Callback

--- src/collision_avoidance.cpp
+++ src/collision_avoidance.cpp
@@ -3,17 +3,45 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Vector3.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <laser_geometry/laser_geometry.h>
+#include <Eigen/StdVector>
+#include <Eigen/Geometry>
+
 
 ros::Publisher cmd_vel_pub;
+bool commandReceived;
+geometry_msgs::Twist latestCommand;
+tf::TransformListener* listener;
+
 
 void safe_cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-  ROS_INFO("Safe Cmd Vel: [x:%f,y:%f,z:%f]", msg->linear.x, msg->linear.y,msg->linear.z);
+  commandReceived = true;
+  //Copy shared pointer (Object will not be destroyed)
+  latestCommand = *msg;
 }
 
 void laser_scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  ROS_INFO("Laser Scan: [%s]", msg->header.frame_id.c_str());
+  if (!commandReceived) return;
+  
+  commandReceived = false;
+
+  laser_geometry::LaserProjection projector;
+  sensor_msgs::PointCloud cloud;
+
+  ROS_INFO("Calculation point cloud");
+  //listener.waitForTransform("/base_link", msg->header.frame_id.c_str(), ros::Time(0), ros::Duration(10.0));
+  projector.transformLaserScanToPointCloud("/base_link", *msg, cloud, *listener);
+
+  for(auto& point : cloud.points){
+    ROS_INFO("x:%f,y:%f,z:%f",point.x,point.y,point.z);
+  }
+
+  cmd_vel_pub.publish(latestCommand);
+
 }
 
 
@@ -22,9 +50,12 @@ int main(int argc, char **argv)
 {
 
   ros::init(argc, argv, "collision_avoidance");
+  commandReceived = false;
 
   ros::NodeHandle n;
 
+  tf::TransformListener lr(ros::Duration(10));
+  listener=&lr;
   //Subscribe to safe_cmd_vel topic
   ros::Subscriber safe_cmd_vel_sub = n.subscribe("safe_cmd_vel", 1000, safe_cmd_vel_callback);
   //Subscribe to laser scan topic
