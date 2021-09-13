2edb6e81a88ef436b2c8f8da13286c50e0f47ff6
dnl-trpp <daniel.trippa@gmail.com>
2021-09-13 16:11:42 +0200

    First working Obstacle detection and avoidance

--- src/collision_avoidance.cpp
+++ src/collision_avoidance.cpp
@@ -8,6 +8,7 @@
 #include <laser_geometry/laser_geometry.h>
 #include <Eigen/StdVector>
 #include <Eigen/Geometry>
+#include <cmath>
 
 
 ros::Publisher cmd_vel_pub;
@@ -15,8 +16,8 @@ bool commandReceived;
 geometry_msgs::Twist latestCommand;
 tf::TransformListener* listener;
 
-
 void safe_cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
+
 {
   commandReceived = true;
   //Copy shared pointer (Object will not be destroyed)
@@ -32,15 +33,36 @@ void laser_scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg)
   laser_geometry::LaserProjection projector;
   sensor_msgs::PointCloud cloud;
 
+  ROS_INFO("Ranges size: %lu",msg->ranges.size());
   ROS_INFO("Calculation point cloud");
   //listener.waitForTransform("/base_link", msg->header.frame_id.c_str(), ros::Time(0), ros::Duration(10.0));
   projector.transformLaserScanToPointCloud("/base_link", *msg, cloud, *listener);
 
-  for(auto& point : cloud.points){
-    ROS_INFO("x:%f,y:%f,z:%f",point.x,point.y,point.z);
-  }
+  //for(auto& point : cloud.points){
+    //ROS_INFO("x:%f,y:%f,z:%f",point.x,point.y,point.z);
+  //}
+  auto obstaclePosition = cloud.points[540];
+  float obstacleDistance = sqrt(obstaclePosition.x*obstaclePosition.x+obstaclePosition.y*obstaclePosition.y); //msg->ranges[540];
+  ROS_INFO("Obstacle Position(In base_link frame): (%f,%f)",obstaclePosition.x,obstaclePosition.y);
+  ROS_INFO("Obstacle Distance: %f",obstacleDistance);
 
-  cmd_vel_pub.publish(latestCommand);
+  if(obstacleDistance<0.5){
+    ROS_INFO("Obstacle in proximity detected!");
+    float forceIntensity= 1.0 / obstacleDistance;
+    float forceX = -(obstaclePosition.x / obstacleDistance) * forceIntensity;
+    float forceY = -(obstaclePosition.y / obstacleDistance) * forceIntensity;
+    ROS_INFO("Repulsing force: (%f,%f)",forceX,forceY);
+    geometry_msgs::Twist cmd_vel;
+    cmd_vel.linear.x = latestCommand.linear.x+forceX;
+    if (signbit(cmd_vel.linear.x)!= signbit(latestCommand.linear.x)) cmd_vel.linear.x = 0;
+    cmd_vel.linear.y = latestCommand.linear.y+forceY;
+    cmd_vel.linear.z = latestCommand.linear.z;
+    cmd_vel.angular  = latestCommand.angular;
+    cmd_vel_pub.publish(cmd_vel);
+  }
+  else{
+     cmd_vel_pub.publish(latestCommand);
+  }
 
 }
 
